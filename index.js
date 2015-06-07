635d02b27eccebeeda6bdc45b869cbdb30a24b1b
Tom Grochowicz <tgrochowicz@gmail.com>
2015-06-07 17:25:52 -0500

    CORS

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.get('/', function(request, response) {
 	response.send('Server-side data retrieval for isthelakefullyet.com');
