4b38fa3636638f42464d37c62cbac02dba39b255
Tom Grochowicz <tgrochowicz@gmail.com>
2015-06-07 18:53:48 -0500

    hosting file in S3

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+var _ = require('lodash');
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
-var fs = require('fs');
+var AWS = require('aws-sdk');
 var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
+
+console.log(AWS_ACCESS_KEY, AWS_SECRET_KEY);
+var s3_params = {
+	Bucket: S3_BUCKET,
+	Key: 'lakedata.json'
+}
+AWS.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+
 
 app.all('/*', function(req, res, next) {
 	// Opening this to the world for now, let's see what happens.
@@ -19,6 +33,15 @@ app.get('/', function(request, response) {
 	response.send('Server-side data retrieval for Lake Travis, TX');
 });
 
+app.get('/lakedata', function(request, response) {
+	var s3 = new AWS.S3();
+	s3.getObject(s3_params, function (err, data) {
+		if (err) response.send(500).end();
+		var body = JSON.parse(data.Body.toString());
+		response.send(body);
+	})
+})
+
 function dataFromColumn($row, column) {
 	return parseFloat($row.find('td:nth-of-type(' + column + ')')
 		.text()
@@ -27,9 +50,14 @@ function dataFromColumn($row, column) {
 
 function insertData(data) {
 	// replace file
-	fs.writeFile('public/lakedata.json', JSON.stringify(data), function(err){
-		console.error(err);
-	});
+
+	var file = _.extend({ Body: JSON.stringify(data) }, s3_params);
+	
+	var s3 = new AWS.S3();
+	s3.upload(file, function (resp) {
+		console.log(resp);
+	})
+	
 }
 
 var pingIntervalId;
